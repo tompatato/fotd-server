5c6b56d3d44dad6588f2fe0c27e18da4c12f9169
Christopher Allford <6451942+ObliviousHarmony@users.noreply.github.com>
2025-08-31 00:40:42 -0700

    Documented Packet Creation Requirements

--- fom-network/include/fom-network/PacketIdentifiers.h
+++ fom-network/include/fom-network/PacketIdentifiers.h
@@ -8,6 +8,15 @@
 #define INVALID_PACKET_ID 0
 
 /**
+ * For every packet identifier, you must also update:
+ *
+ * - include/fom-network/FOMPacket.h: Requires a packet struct.
+ * - src/packet-serializers/PacketSerializers.h: Requires a serializer declaration.
+ * - src/packet-serializers/<PacketName>Serializer.cpp: Requires a serializer implementation.
+ * - src/FOMPacketSerializer.cpp: Requires adding to the serializer map.
+ */
+
+/**
  * @enum PacketIdentifier
  *
  * The identifiers used to indicate how a packet should be serialized and deserialized.
